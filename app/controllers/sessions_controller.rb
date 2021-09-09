36a8b70605a30a4804ff198e24d41a60442e7e37
harada <seungri.uhm@gmail.com>
2021-09-09 12:30:51 +0000

    確認用

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    binding.pry
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user 
+      redirect_to root_path, success: 'ログインに成功しました。'
+    else
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  def session_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
