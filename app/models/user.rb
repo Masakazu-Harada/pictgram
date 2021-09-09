7e04dc39ada3b17d32d2d54e31ccc7739830af9d
harada <seungri.uhm@gmail.com>
2021-09-09 02:37:10 +0000

    Rails 05 User新規登録に潜む問題を解決していくの課題

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15}
+  validates :email, presence: true
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
+  validates :password, presence: true, uniqueness: true, length: {minimum: 8, maximum: 32}, format: {with: VALID_PASSWORD_REGEX, }
+  
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  
+  has_secure_password
+end
