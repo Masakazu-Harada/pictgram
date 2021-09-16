3d2cf7628d625fcf192118a9e9ca5d2f306daa92
harada <seungri.uhm@gmail.com>
2021-09-16 03:53:28 +0000

    課題の画像のサイズ10M以下の部分

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,8 +14,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
   
   def extension_allowlist
-  %w(jpg jpeg gif png)
+    %w(jpg jpeg gif png)
   end
+  
+  def size_range
+    1..10.megabytes
+  end
+    
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
