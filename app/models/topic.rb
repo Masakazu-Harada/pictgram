4d18953f3f83dd813c56f86bda4b3f78a00ea597
harada <seungri.uhm@gmail.com>
2021-09-21 01:54:43 +0000

    正規表現9/21

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,8 @@ class Topic < ApplicationRecord
   validates :description, presence: true
   validates :image, presence: true
 
-  belongs_to :user
-
   mount_uploader :image, ImageUploader
+  
+  has_many :favorites
+  has_many :favorites_user, through: :favorites, source: 'user'
 end
